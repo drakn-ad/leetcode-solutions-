7842794cbe3a1593798360789a25ea7f8e77d457
drakn-ad <adhamgaber55512006@gmail.com>
2026-08-16 23:47:41 +0300

    28. Find the Index of the First Occurrence in a String.java

---
+++ 28. Find the Index of the First Occurrence in a String.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(haystack.contains(needle)){
+            return haystack.indexOf(needle);
+        }
+        return -1;
+    }
+}
+/*
+╔════════════════════╗
+║      DRAKEN        ║
+╚════════════════════╝
+*/
