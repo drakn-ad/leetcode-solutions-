b9dfb46b0ba6343ba4b93a0a8612e4430f4e647e
drakn-ad <adhamgaber55512006@gmail.com>
2026-09-04 23:39:42 +0300

    Create coinChangeTHEOPTIMALESOL.java

---
+++ coinChangeTHEOPTIMALESOL.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+       int[] dp = new int[amount + 1];
+
+    Arrays.fill(dp, amount + 1);
+
+    dp[0] = 0;
+
+    for (int i = 1; i <= amount; i++) {
+
+        for (int c : coins) {
+
+            if (i - c >= 0) {
+                dp[i] = Math.min(dp[i], 1 + dp[i - c]);
+            }
+        }
+    }
+
+    return dp[amount] == amount + 1 ? -1 : dp[amount];
+    }
+}
+
+/*
+STILL CAN NOT UNDERSTAND THIS DAMN
+i'm just asking myself some existencial questions which have no meaning 
+DRAKEN is depressed
+*/
